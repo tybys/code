0e3066358263f92ade0df215abf58826cda4bb42
tybys <gromhs@gmail.com>
2015-11-23 08:49:35 +0300

    columns added

--- slotMachine/app.js
+++ slotMachine/app.js
@@ -68,30 +68,8 @@ $.fn.slotMachine = function(options) {
         data: slotMachineData
     }, options);
 
-    var make = function () {
+    var init = function () {
         var sm = $('#sm');
-        var smCol = $('<div class="sm-col"></div>');
-
-        var elems = Object.keys(slotMachineData).length;
-        var cols = options.drumCount;
-        var total = elems; // ???-?? ?????????
-        var min = Math(floor(total / cols)); // ???. ???-?? ??-?? ? ???????
-        var extra = total - min * cols; // ?????? ??-??
-
-        var i = 0;
-
-        // create columns, depenging on the {drumCount}
-        //var uniqRow = Math.round(rowCount / options.drumCount);
-        function colCreate(colCount) {
-            var i = 0;
-
-            while (i !== colCount) {
-                smCol.removeClass('col-1').removeClass('col-2');
-                $('body').append(smCol.addClass('col-'+ (i+1)).clone());
-                i++;
-            }
-        }
-        colCreate(options.drumCount);
 
         for (var key in slotMachineData) {
             if (slotMachineData.hasOwnProperty(key)) {
@@ -101,36 +79,84 @@ $.fn.slotMachine = function(options) {
                     if (obj.hasOwnProperty(prop)) {
                         //console.log(prop + " = " + obj[prop]);
                         var row = $('<span class="card"></span>');
-                        row.text(obj.text);
-                        row.addClass(key);
+                        row.html("<p>"+obj.text+"</p>");
+                        row.attr("data-name", key);
 
                         var img = $('<img src="'+obj.img+'" />');
                         row.prepend(img);
                     }
                 }
-                // wrap row's in columns, depending on the {drumCount}
-                //row.appendTo($('.sm-col'))
-
-                // DEFAULT STATE WITH TWO DRUMS
-                //if (options.drumCount === 2) {
-                //    if (i >=rowCount / 2) {
-                //        row.appendTo($('.col-2'))
-                //    } else {
-                //        row.appendTo($('.col-1'))
-                //    }
-                //}
+                row.appendTo(sm);
             }
-
-            i++;
         }
 
+        var els = $(this).find('.card');
+        var dimension = els.length;
 
-        console.log('rowCount:'+options.drumCount, 'uniqRow:'+uniqRow)
+        if (dimension > 0) {
+            var elCol = Math.ceil(dimension / options.drumCount);
+            if (elCol < options.drumCount) {
+                elCol = options.drumCount;
+            }
+            var start = 0;
+            var end = elCol;
 
-        //debugger
+            for (var i = 0; i < options.drumCount; i++) {
+                if ((i + 1) == options.columns) {
+                    els.slice(start, end).wrapAll('<div class="wrap last" />');
+                } else {
+                    els.slice(start, end).wrapAll('<div class="wrap" />');
+                }
+                start = start+elCol;
+                end = end+elCol;
+            }
+        }
+
+        var wrap = $('.wrap')
+        $.each(wrap, function (i, v) {
+            var cards = $(v).find('.card');
+            first = cards.eq(0).addClass('first');
+            last = cards.eq(-1).addClass('last');
+        });
+
+        /**
+         * refresh/sort
+         * @type {*|jQuery}
+         */
+        var refresh = $('<a href="#" id="refresh" />').text('refresh').appendTo('body');
+        refresh.on('click', function(e) {
+            e.preventDefault();
+
+            var _this = this; // shitcode
+            //console.log(_this)
+            drumRoll();
+        });
+
+        function drumRoll() {
+            var col = $('#sm').find('.wrap');
+            var heightOffset = 64;
+            var colHeight = col.height();
+
+            var firstTop = first.offset().top;
+            var lastBottom = last.offset().top;
+
+            //if (firstTop > 0) {
+            //    col.animate({
+            //        marginTop: "+="+heightOffset
+            //    });
+            //}
+
+            col.animate({
+                marginTop: "-="+heightOffset
+            });
+
+            console.log(last.offset().top)
+
+
+        }
     };
 
-    return this.each(make);
+    return this.each(init);
 };
 
 $(function () {
