349433b4abfd8ef5504b3b3563989077d9d86fe5
tybys <gromhs@gmail.com>
2015-12-03 08:45:55 +0300

    forms

--- taxi/server.js
+++ taxi/server.js
@@ -1,7 +1,34 @@
-var http = require('http');
+//var http = require('http');
+//
+//var server = http.createServer(function(req, res) {
+//	res.writeHead(200);
+//	res.end('Hello Http');
+//});
+//server.listen('8080');
 
-var server = http.createServer(function(req, res) {
-	res.writeHead(200);
-	res.end('Hello Http');
+//var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/pub'));
+
+console.log(__dirname)
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
 });
-server.listen('8080');
+
+app.get('/drivers', function(req, res) {
+	res.render('pages/drivers');
+});
+
+app.get('transport', function(req, res) {
+	res.render('pages/transport');
+});
+
+app.post('/drivers_add', function (req, res) {
+	res.send('yo')
+});
+
+app.listen(8080);
