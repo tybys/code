f8985a8b0a9aa84bc0d7290dca9682f66a1359a5
tybys <gromhs@gmail.com>
2015-12-05 19:48:43 +0300

    some modules

--- taxi/server.js
+++ taxi/server.js
@@ -11,7 +11,13 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/pub'));
 
-console.log(__dirname)
+var mysql = require('mysql');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
@@ -27,8 +33,45 @@ app.get('transport', function(req, res) {
 	res.render('pages/transport');
 });
 
-app.post('/drivers_add', function (req, res) {
-	res.send('yo')
+
+
+
+function bd() {
+	var connection = mysql.createConnection({
+		user: 'root',
+		password: 'password',
+		host: 'localhost',
+		database: 'taxi'
+	});
+
+	return connection;
+}
+
+app.post('/simple', function (req, res, next) {
+	res.send('success');
+
 });
+
+app.post('/drivers/add_driver', function (req, res, next) {
+	res.send('success');
+
+});
+
+//app.post('/drivers_add', function (req, res) {
+//	var bdRun = bd();
+//
+//	bdRun.query('INSERT INTO driver (name, surname patronymic, birthdate, open_cateory, license, photo, phone_number, comment, status, company_id) values ' +
+//			'("'+req.body.name+'", "'+req.body.surname+'", "'+req.body.patronymic+'", ' +
+//			'"'+req.body.birthdate+'", "'+req.body.open_category+'", "'+req.body.license+'", ' +
+//			'"'+req.body.photo+'", "'+req.body.phone_number+'", "'+req.body.comment+'", ' +
+//			'"'+req.body.status+'", "'+req.body.company_id+'")', function (err, results, fields) {
+//
+//		if (err){
+//			throw err;
+//		} else {
+//			res.send('success');
+//		}
+//	});
+//});
 
 app.listen(8080);
