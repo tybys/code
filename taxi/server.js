5aa4efc51218312989352ec023fa8ef08fa77545
tybys <gromhs@gmail.com>
2016-01-01 20:53:22 +0300

    basic CRUD done

--- taxi/server.js
+++ taxi/server.js
@@ -1,118 +1,43 @@
-//var http = require('http');
-//
-//var server = http.createServer(function(req, res) {
-//	res.writeHead(200);
-//	res.end('Hello Http');
-//});
-//server.listen('8080');
-
-//var http = require('http');
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/pub'));
-
 var mysql = require('mysql');
-
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
-app.use(bodyParser.json());
+//var routes = require('./routes');
+var driver = require('./routes/driver');
+var transport = require('./routes/transport');
+var connection  = require('express-myconnection');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/pub'));
+app.use(connection(mysql, {
+	user: 'root',
+	password: '',
+	host: 'localhost',
+	database: 'taxi'
+}, 'request'));
 app.set('view engine', 'ejs');
+app.locals.title;
 
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
 
-app.get('/drivers', function(req, res) {
-	var bdRun = bd();
-	bdRun.query('SELECT * FROM driver', function (err, results, fields) {
-		if (err){
-			throw err;
-		} else {
-			//res.send('success');
-			//res.send(results);
-			//console.log(results[0].name)
-			res.render('pages/drivers', {data: results});
-		}
-	});
-});
 
-app.get('/drivers/edit/:id', function (req, res, err) {
-	var id = req.params.id;
-	var bdRun = bd();
-	var backURL = req.header('Referer') || '/';
+// DRIVER
+app.get('/drivers', driver.list);
+app.get('/drivers/edit/:id', driver.edit);
+app.post('/drivers/edit/:id', driver.save);
+app.get('/drivers/del/:id', driver.delete);
+app.post('/drivers/add_driver', driver.add);
+// END DRIVER
 
-	bdRun.query('SELECT * FROM driver WHERE id = ?', [id], function (err, rows) {
-		if (err) throw err;
-		res.render('pages/edit', {data: rows});
-	});
-});
-
-app.post('/drivers/edit/:id', function (req, res, err) {
-	var id = req.params.id;
-	var bdRun = bd();
-	var backURL = req.header('Referer') || '/';
-
-	bdRun.query('UPDATE driver SET ? WHERE ?', [{Name: req.body.name_upd}, {Id: id}]), function (err, results, fields) {
-		if (err) throw err;
-		else {
-			//
-			// res.setHeader("Content-Type", "text/html");
-			//res.writeHead(200, { 'Content-Length': body.length, 'Content-Type': 'text/plain' });
-			res.sendStatus(302);
-		}
-	}
-});
-
-app.get('/drivers/del/:id', function (req, res, err) {
-	var id = req.params.id;
-	var bdRun = bd();
-	var backURL = req.header('Referer') || '/';
-
-	bdRun.query("DELETE FROM driver WHERE id = ? ", [id], function (err, rows) {
-		if (err) {
-			throw err;
-		} else {
-			res.redirect(backURL);
-		}
-	});
-});
-
-app.get('transport', function(req, res) {
-	res.render('pages/transport');
-});
-
-function bd() {
-	var connection = mysql.createConnection({
-		user: 'root',
-		password: '',
-		host: 'localhost',
-		database: 'taxi'
-	});
-
-	return connection;
-}
-
-app.post('/drivers/add_driver', function (req, res, next) {
-	var bdRun = bd();
-	var backURL = req.header('Referer') || '/';
-
-	bdRun.query('INSERT INTO driver (name, surname, patronymic, birthdate, open_category, license, photo, phone_number, comment, status, company_id) values ' +
-			'("'+req.body.name+'", "'+req.body.surname+'", "'+req.body.patronymic+'", ' +
-			'"'+req.body.birthdate+'", "'+req.body.open_category+'", "'+req.body.license+'", ' +
-			'"'+req.body.photo+'", "'+req.body.phone_number+'", "'+req.body.comment+'", ' +
-			'"'+req.body.status+'", "'+req.body.company_id+'")', function (err, results, fields) {
-
-		if (err){
-			throw err;
-		} else {
-			//res.send('success');
-			res.redirect(backURL);
-		}
-	});
-
-});
+// TRANSPORT
+app.get('/transport', transport.list);
+app.get('/transport/edit/:id', transport.edit);
+app.post('/transport/edit/:id', transport.save);
+app.get('/transport/del/:id', transport.delete);
+app.post('/transport/add_transport', transport.add);
+// END TRANSPORT
 
 app.listen(80);
